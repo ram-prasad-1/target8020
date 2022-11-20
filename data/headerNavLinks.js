de9a6599cbc658876ff3d1308deb52ebf285f8c4
ram-prasad-1 <ramprasaddeora@gmail.com>
2022-11-20 21:21:25 +0530

    few changes

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Writing' },
-  { href: '/tutorials', title: 'Tutorials' },
-  { href: '/tags', title: 'Tags' },
+  { href: '/tutorial/javascript', title: 'Tutorials' },
+  // { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  // { href: '/about', title: 'About' },
 ];
 
 export default headerNavLinks;
