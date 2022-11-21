0f58fc07d0e0c94f2e4e8e7412b99b9d2495568a
ram-prasad-1 <ramprasaddeora@gmail.com>
2022-11-21 06:25:09 +0530

    blog setup complete

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Writing' },
-  { href: '/tutorial/javascript', title: 'Tutorials' },
+  { href: '/tutorials', title: 'Tutorials' },
   // { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   // { href: '/about', title: 'About' },
