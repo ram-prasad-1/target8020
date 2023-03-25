a5b8721cd3a7803e5acf66bde17e5ee25c57e806
ram-prasad-1 <ramprasaddeora@gmail.com>
2023-03-25 19:45:43 +0530

    add obsidian

--- src/components/LayoutWrapper.js
+++ src/components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata';
 import headerNavLinks from '@/data/headerNavLinks';
-import Logo from '@/data/ellipsoidal-reflector.svg';
+import Logo from '@/static/ellipsoidal-reflector.svg';
 import Link from './Link';
 import SectionContainer from './SectionContainer';
 import Footer from './Footer';
